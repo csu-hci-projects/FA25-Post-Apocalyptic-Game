5466a1715210ce3ee5ab39264d7c4fa071128f06
Nikolas Augustini <nikolas.augustini@gmail.com>
2025-12-01 18:43:04 -0700

    Updated volume scripting.
    Updated menuController script to allow for the volume and text in the popout to adjust.

--- Per Kehrem/Assets/Scripts/MenuController.cs
+++ Per Kehrem/Assets/Scripts/MenuController.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
 
 public class Exit : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volTextVal = null;
+    [SerializeField] private Slider volSlider = null;
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels To Load")]
     public string newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("masterVolume"))
+        {
+            float savedVolume = PlayerPrefs.GetFloat("masterVolume");
+            AudioListener.volume = savedVolume;
+            volSlider.value = savedVolume;
+            volTextVal.text = savedVolume.ToString("0%");
+        }
+
+        volSlider.onValueChanged.AddListener(SetVolume);
+    }
 
     public void NewGameDialogYes()
     {
@@ -16,7 +36,7 @@ public class Exit : MonoBehaviour
 
     public void LoadGameDialogYes()
     {
-        if(PlayerPrefs.HasKey("SavedLevel"))
+        if (PlayerPrefs.HasKey("SavedLevel"))
         {
             levelToLoad = PlayerPrefs.GetString("SavedLevel");
             SceneManager.LoadScene(levelToLoad);
@@ -30,5 +50,25 @@ public class Exit : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volTextVal.text = volume.ToString("0.0") + "%";
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
